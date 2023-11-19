42ad7e37287f4dcfbfb61fb55ee198c17a32a96c
bskydr <62293824+bskydr@users.noreply.github.com>
2023-11-19 12:29:19 -0400

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import "alpinejs";
+import EmblaCarousel from "embla-carousel";
+import "../css/base.css";
+import "../css/reset.css";
+import "../css/embla.css";
+import "../css/header.css";
+import "../css/footer.css";
+
+window.EmblaCarousel = EmblaCarousel;
